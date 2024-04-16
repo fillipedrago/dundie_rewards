1d6ee9ae51890cb1bf3f69a8fdb07b0d90049b96
Fillipe Drago <fillipedrago@gmail.com>
2024-04-16 18:39:51 -0300

    Modificações no script

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,4 +1,44 @@
+import argparse
+
+def load(filepath):
+    """Loads data from filepath to the database"""
+    try:
+        with open(filepath) as file_:
+            for line in file_:
+                print(line)
+    except FileNotFoundError as e:
+        print(f"File not found: {e}")
 
 
 def main():
+    parser = argparse.ArgumentParser(
+    description= "Dundie Mifflin Rewards CLI",
+    epilog="Enjoy and use with caution",
+    )
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load", "show", "send"),
+        default="help",
+        required=False
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="Filepath to load",
+        default="help",
+        default=None
+    )
+    args = parser.parse_args()
+
+    try:
+        globals()[args.subcommand](args.filepath)
+    except KeyError:
+        print("Subcommand is invalid")
+
     print("Executing entry point for dundie...")
+
+
+if __name__ == "__main__":
+    main()
