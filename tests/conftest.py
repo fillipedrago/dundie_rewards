2cf376a3c7ed7afe734333a55565a7053e194703
Fillipe Drago <fillipedrago@gmail.com>
2024-04-19 18:47:45 -0300

    Boas práticas testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,14 @@ high: High Priority
 medium: Medium Priority
 low: Low Priority
 """
+import pytest
 
 def pytest_configure(config):
     for line in MARKER.split("\n"):
         config.addinivalue_line("markers", line)
+
+@pytest.fixture(autouse=True)
+def go_to_tmpdir(request): # injeção de dependencias
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield # protocolo de generators
